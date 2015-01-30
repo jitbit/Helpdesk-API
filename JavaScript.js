49d0678e3862cad4dae0d8c8fd30a47a40a83963
jitbit <jitbit@gmail.com>
2015-01-30 23:16:29 +0300

    JS sample, modified README

---
+++ JavaScript.js
@@ -0,0 +1,23 @@
+//This is a basic example of using Jitbit Helpdesk API with jQuery
+
+//Creates a base-64 encoded Authorization header value
+var makeBasicAuthHeader = function(username, password) {
+	return "Basic " + btoa(username + ":" + password);
+}
+
+//Set's default authorization header for all jQuery AJAX requests
+$.ajaxSetup({
+	headers: { 'Authorization': makeBasicAuthHeader('admin', 'admin') }
+});
+
+//Gets the list of unanswered tickets for the authenticated user
+//Replace "XXXXX" with your helpdesk URL
+$.get("https://XXXXX.jitbit.com/helpdesk/api/tickets",
+	function(tickets){
+		if(tickets.length > 0){
+			$.each(tickets, function(index, val) {
+				console.log(val.Subject);
+			});
+		}
+	}
+);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Helpdesk-API
+# Jitbit Helpdesk Ticketing System API
+
+This is an open-source collection of code-snippets for [Jitbit Helpdesk](https://www.jitbit.com/web-helpdesk/ "Helpdesk Ticketing System") that shows how you can use the helpdesk app's [API](https://www.jitbit.com/web-helpdesk/helpdesk-api/).
+
+Feel free to contribute your samples.
