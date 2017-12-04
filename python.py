0946e26710fa4883adb1978606fa767a2dd0cc2a
Alex Yumashev <33555768+alex-jitbit@users.noreply.github.com>
2017-12-04 22:12:58 +0200

    Create python.py

---
+++ python.py
@@ -0,0 +1,10 @@
+import json
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+# lets make a request
+url = "http://XXX.jitbit.com/helpdesk/api/tickets"
+authentication = HTTPBasicAuth(username, password)
+response = requests.get(url, auth=authentication)
+result = json.loads(response.content)
